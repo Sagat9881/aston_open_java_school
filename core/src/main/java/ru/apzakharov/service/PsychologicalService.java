fce5a1c4eab0a8daf345b792f96943d7dd014904
apzakharov <apzakharov9881@gmail.com>
2024-02-17 23:59:33 +0300

    Добавление модуля БД к сервисному слою

--- core/src/main/java/ru/apzakharov/service/PsychologicalService.java
+++ core/src/main/java/ru/apzakharov/service/PsychologicalService.java
@@ -1,9 +1,18 @@
 package ru.apzakharov.service;
 
 import ru.apzakharov.healing.PsychologicalHealing;
+import ru.apzakharov.mydbms.service.QueryService;
+
+import java.util.List;
+import java.util.Map;
 
 public class PsychologicalService implements HealthService<PsychologicalHealing> {
 
+    private final QueryService<PsychologicalHealing, List<Map<String, Object>>> dao;
+
+    public PsychologicalService(QueryService<PsychologicalHealing, List<Map<String, Object>>> dao) {
+        this.dao = dao;
+    }
 
     @Override
     public PsychologicalHealing getSomeHeal() {
