b4ef8bae7d326d317120f1aba6109e1296835c75
mikon <mikon051@mail.ru>
2024-02-23 02:23:16 +0300

    Удаление сгенирированого теста

--- src/main/java/HealthServlet.java
+++ src/main/java/HealthServlet.java
@@ -3,14 +3,26 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Random;
 
 public class HealthServlet extends HttpServlet {
+    final String [] phrases;
+
+    public HealthServlet() {
+        this.phrases = new String[] {"Будь здоров"};
+    }
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doGet(req,resp);
+        try (PrintWriter writer = resp.getWriter()){
+            Random random = new Random();
+            writer.write(phrases[random.nextInt(phrases.length)]);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doPost(req,resp);
     }
 }
